3a0b209b9ff2835630ef59baff8d2714337ddae7
zoobox-cyber <bitimibop@gmail.com>
2024-09-11 10:25:39 -0400

    More corrections

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-# Steps for setting up the system
-#
-# sudo apt update
-# sudo apt upgrade
-# sudo apt install git -y
-# git clone https://github.com/zoobox-cyber/zoobox.git
-# chmod +x zoobox/setup.sh
-# zoobox/setup.sh
+# Steps for setting up the system on a fresh debian install
+# 
+#   Getting the files for the installation of zoobox
+#     sudo apt update
+#     sudo apt install git -y
+#     git clone https://github.com/zoobox-cyber/zoobox.git
+# 
+#   Executing the installation script
+#     chmod +x zoobox/setup.sh
+#     zoobox/setup.sh
+# 
+#   User input necessary
+#     After a few minutes, the installation of
+#     lightdm triggers an event requiring user input.
+#     On the first popup window, select 'OK' by pressing ENTER.
+#     On the second popup window, select lightdm.
+#     The installation should proceed automatically until
+#     completely finished.
+# 
+#   Setting Crux as the GTK theme
+#     In a terminal run the following
+#       lxappearance   
+#     A window will open. Select Crux in the left side bar
+#     close the app.
+# 
+# Reboot to see changes.
+# 
+# 
+# 
+# 
+ 
+
 
--- setup.sh
+++ setup.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
 
-# Update and upgrade system packages
+# Update system packages
 echo "Updating system packages..."
-sudo apt update && sudo apt upgrade -y
+sudo apt update -y
 
 # Install core dependencies
 echo "Installing core dependencies..."
-sudo apt install -y git curl terminator openbox htop qt5ct lxappearance lightdm xdotool
+sudo apt install -y git terminator openbox qt5ct lxappearance lightdm xdotool
 
 # Install utilities
-echo "Installing core dependencies..."
-sudo apt install -y yt-dlp audacity kdenlive vlc
-
-# Clone your repository (if not already done)
-REPO_URL="https://github.com/zoobox-cyber/zoobox.git"
-REPO_DIR="zoobox"
-
-if [ ! -d "$REPO_DIR" ]; then
-  echo "Cloning repository..."
-  git clone $REPO_URL
-else
-  echo "Repository already cloned. Pulling latest changes..."
-  cd $REPO_DIR
-  git pull origin main
-  cd ..
-fi
+# echo "Installing utilities..."
+# sudo apt install -y yt-dlp audacity kdenlive vlc curl htop 
 
 # Copy configuration files
-echo "Setting up Openbox configurations..."
+echo "Setting up base configuration..."
 cp -r $REPO_DIR/home/.config/* ~/.config/
   # Openbox theme
-cp -r $REPO_DIR/home/.themes/* ~/.themes/
+cp -r $REPO_DIR/home/.themes/Penumbra ~/.themes/
   # GTK theme
 sudo cp -r $REPO_DIR/usr/share/themes/Crux /usr/share/themes/
-  # Background
-sudo cp -r $REPO_DIR/boot/grub/black.jpg /boot/grub/
+  # Session setup
+sudo cp $REPO_DIR/boot/grub/black.jpg /boot/grub/
+sudo cp $REPO_DIR/usr/share/lightdm/lightdm-gtk-greeter.conf.d/01_debian.conf /usr/share/lightdm/lightdm-gtk-greeter.conf.d/01_debian.conf
+	
 
 # Final message
-echo "Setup completed. Please reboot to apply all changes."
+echo "Installation completed. Please follow the next steps in the README file to finish set up."
 
 
