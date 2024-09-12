dcf99622c81488a24994cb68e032588bcdcacac7
zoobox-cyber <bitimibop@gmail.com>
2024-09-12 10:09:02 -0400

    Set Openbox as default in lightdm

--- setup.sh
+++ setup.sh
@@ -30,7 +30,12 @@ sudo apt install -y lightdm
 sudo apt install -y lightdm-settings
 sudo cp -r zoobox/etc/lightdm /etc/lightdm
 sudo cp zoobox/usr/share/images/desktop-base/black.jpg /usr/share/images/desktop-base/black.jpg
-sudo cp zoobox/usr/share/lightdm/lightdm-gtk-greeter.conf.d/01_debian.conf /usr/share/lightdm/lightdm-gtk-greeter.conf.d/01_debian.conf	
+sudo cp zoobox/usr/share/lightdm/lightdm-gtk-greeter.conf.d/01_debian.conf /usr/share/lightdm/lightdm-gtk-greeter.conf.d/01_debian.conf
+
+# Configure LightDM to use Openbox by default
+echo "Configuring LightDM to use Openbox by default..."
+sudo bash -c 'echo "[Seat:*]
+user-session=openbox" > /etc/lightdm/lightdm.conf.d/openbox.conf'
 
 # Final message
 echo "Installation completed. Please follow the next steps in the README file to finish the set up."
